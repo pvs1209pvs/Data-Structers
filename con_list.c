c41d00c9ac17b2f18f9b7d92e4d4d8a58c2cb994
Paramvir <ps17cw@brocku.ca>
2021-01-30 19:46:31 -0500

    con_list bugs fixed and refactored

--- con_list.c
+++ con_list.c
@@ -2,52 +2,101 @@
 #include "con_list.h"
 
 
-void con_list_init(struct List *list, size_t cpcty) {
+/**
+ * Initializes con_list.
+ * @param list List that has to be initialized.
+ * @param cpty How many elements list can hold.
+*/
+void con_list_init(struct List *list, const size_t cpcty) {
   list->head = malloc(cpcty * sizeof(void*));
   list->cpcty = cpcty;
   list->size = 0;
 }
 
 
-void con_list_add(struct List *list, void *ele) {
+/**
+ * Appends elements at the end of the list.
+ * @param list List to which elements will be appended.
+ * @param ele Address of the elements that the list will be storing.
+*/
+void con_list_add(struct List *list, void * const ele) {
   if (list->size == list->cpcty) con_list_grow(list);
   list->head[list->size++] = ele;
-  //*(list->head + list->size++) = ele;
 }
 
 
-void con_list_dltIndx(struct List *list, size_t i) {
-  free(list->head[i]);
-  for (size_t j = i; j < list->size-1; ++j)
-      list->head[j] = list->head[j + 1];
-  list->head[list->size - 1] = NULL;
+/**
+ * Deletes the element at the valid index.
+ * @param list List from whic the element will be deleted.
+ * @parem i Index of the element that has to be deleted.
+*/
+void * con_list_dlt_indx(struct List * const list, const size_t i) {
+
+  void * r = list->head[i];
+
+  for (size_t j = i; j < list->size-1; ++j){
+    list->head[j] = list->head[j + 1];
+  }
+
   list->size--;
+
+  return r;
+
 }
 
 
-void *con_list_get(struct List *list, size_t i) {
+/**
+ * Returns the element from a valid index.
+ * @param list List from which the element has to be returned.
+ * @param i Index of the element to be returned.
+*/
+void *con_list_get(const struct List * const list, const size_t i) {
   return list->head[i];
 }
 
 
-size_t con_list_size(struct List *list) {
+/**
+ * Returns the size of the list.
+ * @param list List whose size we wish get.
+*/
+size_t con_list_size(const struct List * const list) {
   return list->size;
 }
 
 
-void con_list_clr(struct List *list) {
-  for (int i = 0; i < list->size; ++i)
-    free(list->head[i]);
-  list->cpcty = 10;
+/**
+ * Removes all the elements from the list.
+ * @param list List to be cleared.
+*/
+void con_list_clr(struct List * const list) {
+
+  for (int i = 0; i < list->size; ++i){
+    con_list_dlt_indx(list, i);
+  }
+
   list->size = 0;
+
 }
 
 
-void con_list_setAt(struct List *list, size_t i, void *ele) {
+/**
+ * Adds the element at the valid index.
+ * @param list List in which element is to be added.
+ * @param i Index at which the element has to be added.
+ * @param ele: ele that is be added at the index i.
+*/
+void con_list_set_at(const struct List * const list, size_t i, void * const ele) {
   list->head[i] = ele;
 }
 
 
+/**
+ * Checks if the list contains the desired element.
+ * @param list List in which the presence of the element has to checked.
+ * @param ele Element that to be checked for.
+ * @param compareTo function poninter that dictates how the elements should be compared.
+ * @return 1 is the element is present in the list, else 0.
+*/
 _Bool con_list_cntns(struct List *list, void *ele, int(compareTo)(const void * const x, const void * const y)) {
   for (int i = 0; i < list->size; ++i) {
     if (compareTo(list->head[i], ele) == 0) return 1;
@@ -56,17 +105,30 @@ _Bool con_list_cntns(struct List *list, void *ele, int(compareTo)(const void * c
 }
 
 
+/**
+ * Checks if the list is empty.
+ * @param list List that has to be checked.
+ * @return 1 is the list is empty, else 0.
+*/
 _Bool con_list_isEmpty(struct List *list) {
   return list->size == 0;
 }
 
 
+/**
+ * Doubles the capacity of the list once it is full.
+ * @param list List whose capacity has to be doubled.
+*/
 void con_list_grow(struct List *list) {
   list->cpcty = 2 * list->cpcty;
   list->head = realloc(list->head, list->cpcty * sizeof(void*));
 }
 
 
+/**
+ * Removes list from the heap.
+ * @param list List that needs to be freeed.
+*/
 void con_list_free(struct List *list) {
   free(list->head);
 }
--- test.c
+++ test.c
@@ -1,62 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "binary_tree.h"
-#include "map.h"
 #include "con_list.h"
 
-int compare_to(const void * const x,  const void * const y){
 
-    struct Pair * a = (struct Pair*)x;
-    struct Pair * b = (struct Pair*)y;
-    return *(int*)a->key -  *(int*)b->key;
-
-}
 
 int main() {
 
-    int keys[] = {1,2,3,4,5};
-    int values[] = {10,20,30,40,50};
+    struct List myList;
+    con_list_init(&myList, 2);
 
-    struct Pair int_pairs[] = { 
-        {.key=&keys[0], .value=&values[0]},
-        {.key=&keys[1], .value=&values[1]},
-        {.key=&keys[2], .value=&values[2]},
-        {.key=&keys[3], .value=&values[3]},
-        {.key=&keys[4], .value=&values[4]},
-        };
+    int a, b, c, d;
+    a = 5;
+    b = 10;
+    c = 15;
+    d = 20;
 
+    con_list_add(&myList, &a);
+    con_list_add(&myList, &b);
+    con_list_add(&myList, &c);
+    con_list_add(&myList, &d);
 
-    struct Map * mapo = map_init(int_pairs[0]);
+   (con_list_dlt_indx(&myList, 2));
 
-    map_add(mapo, int_pairs[1], compare_to);
-    map_add(mapo, int_pairs[2], compare_to);
-    map_add(mapo, int_pairs[3], compare_to);
+    for (size_t i = 0; i < con_list_size(&myList); i++){
+        printf("%d \n", *(int*)con_list_get(&myList, i));
+    }
+
     
-
-    // char * f_name = "param";
-    // char * l_name = "singh";
-    // struct Pair name = {f_name, l_name};
-
-    // printf("%s %s\n", (char*)name.key, (char*)name.value);
-
-    // struct Map * map = map_init(name);
-
-    // struct Pair * p = (struct Pair*)map->bnry_tree->key;
-    // printf("%s %s\n", (char*)p->key, (char*)p->value);
-
-    //printf("%s \n", map->bnry_tree->key);
     
-    // int keys[] = {1, 2, 5, 3, 4, 6, 7, 9, 8, 10};
-
-    // struct Binary_Node * root = binary_tree_init_node(&keys[0]);
-
-    // for (size_t i = 1; i < sizeof(keys)/sizeof(keys[0]); i++){
-    //       binary_tree_add(root, binary_tree_init_node(&keys[i]), compare_to);
-    // }
-    
-
-    // struct List l = binary_tree_ele_array(root);
+    free(myList.head);
     
     
 
